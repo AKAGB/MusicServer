11e72002e38eccbd9b25706b9dd284e8c8fe760f
AKAGB <374118168@qq.com>
2019-01-14 21:16:45 +0800

    Add Data

--- MusicServer/Server/admin.py
+++ MusicServer/Server/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(AppUser)
+admin.site.register(Singer)
+admin.site.register(Company)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(PlayList)
--- MusicServer/Server/models.py
+++ MusicServer/Server/models.py
@@ -5,17 +5,26 @@ from django.contrib.auth.models import User
 class AppUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=20)
-    reg_time = models.CharField(max_length=10)
+    reg_time = models.DateField()
     user_place = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user_name
 
 class Singer(models.Model):
     singer_name = models.CharField(max_length=20)
     singer_en_name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.singer_name
+
 class Company(models.Model):
     company_name = models.CharField(max_length=20)
     build_date = models.DateField()
     company_place = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.company_name
 
 class Album(models.Model):
     album_name = models.CharField(max_length=20)
@@ -26,16 +35,20 @@ class Album(models.Model):
     # Relationship set
     users = models.ManyToManyField(AppUser)
 
+    def __str__(self):
+        return self.album_name
+
 class Song(models.Model):
     music_name = models.CharField(max_length=20)
+    music_url = models.URLField(default='')
     music_time = models.CharField(max_length=10)
     music_year = models.IntegerField()
     music_style = models.CharField(max_length=10)
     singer_id = models.ForeignKey(Singer, on_delete=models.CASCADE, default=1)
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE, default=1)
 
-
-
+    def __str__(self):
+        return self.music_name
 
 class PlayList(models.Model):
     list_name = models.CharField(max_length=20)
@@ -47,3 +60,5 @@ class PlayList(models.Model):
     user = models.ManyToManyField(AppUser)
     songs = models.ManyToManyField(Song)
 
+    def __str__(self):
+        return self.list_name
